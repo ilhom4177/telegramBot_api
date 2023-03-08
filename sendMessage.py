3b22c3d18c60982d6dc14c7f2ef3b687321989e3
ilhom <ilhommamatqulov276@gmail.com>
2023-03-08 16:56:40 +0500

    sendMessage.py

--- sendMessage.py
+++ sendMessage.py
@@ -2,8 +2,16 @@ import os
 import requests
 
 TOKEN = os.environ['TOKEN']
-
+    
+    
 def sendMessage(chat_id:str, text:str):
     URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
-    
-    
+    payload = {
+        'chat_id':chat_id, 
+        'text':text,
+        'parse_mode':'HTML'}
+    response = requests.post(url=URL, data=payload)
+    return response.json()
+
+if __name__ == "__main__":
+    print(sendMessage(5575549228, "<b>Hello</b> <i>HELLO</i> <a href='google.com'>GOOGLE</a>"))
